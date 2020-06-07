aebbcaf93a705cd919feb0bb7cd77f5503b65c9d
JayeshPadhiar <jayeshpadhiar20@gmail.com>
2020-06-07 18:04:22 +0530

    Updates

---
+++ precache-manifest.020c4d8552af8302d677e57ccfeba507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc7950c07ce5fda5df30e204f6a5007",
+    "url": "/TheWeather/index.html"
+  },
+  {
+    "revision": "d8157c0774e656a2c793",
+    "url": "/TheWeather/static/css/main.df09d32e.chunk.css"
+  },
+  {
+    "revision": "9862ed9d843505972839",
+    "url": "/TheWeather/static/js/2.6fa157d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TheWeather/static/js/2.6fa157d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8157c0774e656a2c793",
+    "url": "/TheWeather/static/js/main.f97663bb.chunk.js"
+  },
+  {
+    "revision": "d13c7372af6c0c8d8176",
+    "url": "/TheWeather/static/js/runtime-main.ad248f0e.js"
+  }
+]);
